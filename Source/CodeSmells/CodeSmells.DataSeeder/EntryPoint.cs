a7bce9543913f1ec1ee2eb938bae0d6f50011ca7
vladislavVI <vladkohackera@gmail.com>
2014-10-21 22:45:59 +0300

    Started DataSeeder project
    Created a new project, which will servve for filling the db with data.

---
+++ Source/CodeSmells/CodeSmells.DataSeeder/EntryPoint.cs
@@ -0,0 +1,39 @@
+﻿namespace CodeSmells.DataSeeder
+{
+    using System;
+    using FileSniffing;
+
+    class EntryPoint
+    {
+        static void SniffForCodeFiles() 
+        {
+            //Sniff folders for code! 
+            string[] smellyDirs = new string[]{
+                     @"E:\xampp\htdocs\",
+                     @"E:\GitHub\personal\personal\TelerikAcademy\"
+                };
+            //specify extensions
+            string[] allowedExtensions = new string[] { "*.php", "*.js", "*.html", "*.cs", "*.css" };
+            CodeFileSniffer fileSniffer = new CodeFileSniffer(smellyDirs, allowedExtensions, "smellyFiles.txt");
+            Console.WriteLine("Started file sniffing...");
+            fileSniffer.Sniff();
+            Console.WriteLine("File sniffing finished!");
+        }
+
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("CodeSmells data seeder v1.0");
+                Console.WriteLine("- - -");
+                //First: Sniff for code files.
+                //SniffForCodeFiles();
+                //Second: Extract code snippets.
+            }
+            catch (Exception boom) 
+            {
+                Console.WriteLine("[!] Ups, something went wrong! "+boom.Message);
+            }
+        }
+    }
+}
